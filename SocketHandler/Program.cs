9fbb91876e189741f2925999c945a333518ea8e5
fyrkantis <david@kniberg.com>
2022-03-21 10:24:00 +0100

    Moved socket methods to new file.

--- SocketHandler/Program.cs
+++ SocketHandler/Program.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 static class Program
 {
-    // https://stackoverflow.com/a/722265/13347795
-    public static bool IsConnected(this Socket socket)
-    {
-        try
-        {
-            return !(socket.Poll(1, SelectMode.SelectRead) && socket.Available == 0);
-        }
-        catch (SocketException)
-        {
-            return false;
-        }
-    }
     static void Main()
     {
         IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
---
+++ SocketHandler/SocketMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Sockets;
+
+static class SocketMethods {
+    // https://stackoverflow.com/a/722265/13347795
+    public static bool IsConnected(this Socket socket)
+    {
+        try
+        {
+            return !(socket.Poll(1, SelectMode.SelectRead) && socket.Available == 0);
+        }
+        catch (SocketException)
+        {
+            return false;
+        }
+    }
+
+    /*public static HttpRequestHeaders ReadHeaders(this Socket socket)
+    {
+        
+    }*/
+}
